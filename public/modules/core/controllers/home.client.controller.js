3c005990bfdab89408abb05202d399d5d688cd9f
Xuebin Dong <xudong@redhat.com>
2015-03-27 15:53:58 +0800

    [in progress]add slides for main page

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -2,8 +2,25 @@
 
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-	}
+  function($scope, Authentication) {
+    // This provides Authentication context.
+    $scope.authentication = Authentication;
+  }
 ]);
+
+angular.module('core').controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 1000 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+  $scope.next = function() { alert('OK'); };
+});
