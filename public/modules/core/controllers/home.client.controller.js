8296d531faff0f943926e68be31358228ba557f6
Xuebin Dong <xudong@redhat.com>
2015-04-02 10:53:02 +0800

    redirect users to the activities list page after login

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -12,36 +12,31 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
       { image: 'modules/core/img/slides/02.jpg', text: 'Dida' },
       { image: 'modules/core/img/slides/03.jpg', text: 'Dida' }];
 
-  $scope.icons = [{ src: 'modules/core/img/cohort/01.jpg', name: 'haoran'},
-    { src: 'modules/core/img/cohort/02.jpg' },
-    { src: 'modules/core/img/cohort/03.jpg' },
-    { src: 'modules/core/img/cohort/04.jpg' },
-    { src: 'modules/core/img/cohort/05.jpg' },
-    { src: 'modules/core/img/cohort/06.jpg' },
-    { src: 'modules/core/img/cohort/07.jpg' },
-    { src: 'modules/core/img/cohort/08.jpg' },
-    { src: 'modules/core/img/cohort/09.jpg' },
-    { src: 'modules/core/img/cohort/10.jpg' },
-    { src: 'modules/core/img/cohort/11.jpg' },
-    { src: 'modules/core/img/cohort/12.jpg' },
-    { src: 'modules/core/img/cohort/13.jpg' },
-    { src: 'modules/core/img/cohort/14.jpg' },
-    { src: 'modules/core/img/cohort/15.jpg' },
-    { src: 'modules/core/img/cohort/16.jpg' },
-    { src: 'modules/core/img/cohort/17.jpg' },
-    { src: 'modules/core/img/cohort/18.jpg' },
-    { src: 'modules/core/img/cohort/19.jpg' },
-    { src: 'modules/core/img/cohort/20.jpg' },
-    { src: 'modules/core/img/cohort/21.jpg' },
-    { src: 'modules/core/img/cohort/22.jpg' },
-    { src: 'modules/core/img/cohort/23.jpg' },
-    { src: 'modules/core/img/cohort/24.jpg' }];
-
+    $scope.icons = [{ src: 'modules/core/img/cohort/01.jpg', name: 'haoran'},
+      { src: 'modules/core/img/cohort/02.jpg' },
+      { src: 'modules/core/img/cohort/03.jpg' },
+      { src: 'modules/core/img/cohort/04.jpg' },
+      { src: 'modules/core/img/cohort/05.jpg' },
+      { src: 'modules/core/img/cohort/06.jpg' },
+      { src: 'modules/core/img/cohort/07.jpg' },
+      { src: 'modules/core/img/cohort/08.jpg' },
+      { src: 'modules/core/img/cohort/09.jpg' },
+      { src: 'modules/core/img/cohort/10.jpg' },
+      { src: 'modules/core/img/cohort/11.jpg' },
+      { src: 'modules/core/img/cohort/12.jpg' },
+      { src: 'modules/core/img/cohort/13.jpg' },
+      { src: 'modules/core/img/cohort/14.jpg' },
+      { src: 'modules/core/img/cohort/15.jpg' },
+      { src: 'modules/core/img/cohort/16.jpg' },
+      { src: 'modules/core/img/cohort/17.jpg' },
+      { src: 'modules/core/img/cohort/18.jpg' },
+      { src: 'modules/core/img/cohort/19.jpg' },
+      { src: 'modules/core/img/cohort/20.jpg' },
+      { src: 'modules/core/img/cohort/21.jpg' },
+      { src: 'modules/core/img/cohort/22.jpg' },
+      { src: 'modules/core/img/cohort/23.jpg' },
+      { src: 'modules/core/img/cohort/24.jpg' }];
 
   }
-
-
-
-
 
 ]);
