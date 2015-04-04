17320f922831f45f00a5b25b166155e967f576cf
dabeng <xinguan413@163.com>
2015-04-04 10:45:17 +0800

    fix-bug: if users didn't change date and time fields, the created activity disn't has the dt propety

--- public/modules/activities/controllers/activities.client.controller.js
+++ public/modules/activities/controllers/activities.client.controller.js
@@ -2,68 +2,69 @@
 
 // Activities controller
 angular.module('activities').controller('ActivitiesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Activities',
-	function($scope, $stateParams, $location, Authentication, Activities) {
-		$scope.authentication = Authentication;
+  function($scope, $stateParams, $location, Authentication, Activities) {
+    $scope.authentication = Authentication;
 
-		$scope.datetime = { holdDate: '', holdTime: ''};
+    $scope.datetime = { holdDate: (new Date()).toDateString(),
+      holdTime: (new Date()).toTimeString() };
 
-		// Create new Activity
-		$scope.create = function() {
-			// Create new Activity object
-			var activity = new Activities ({
-				name: this.name,
-				dateTime: new Date(this.datetime.holdDate + ' ' + this.datetime.holdTime)
-			});
+    // Create new Activity
+    $scope.create = function() {
+      // Create new Activity object
+      var activity = new Activities ({
+        name: this.name,
+        dateTime: new Date(this.datetime.holdDate + ' ' + this.datetime.holdTime)
+      });
 
-			// Redirect after save
-			activity.$save(function(response) {
-				$location.path('activities/' + response._id);
+      // Redirect after save
+      activity.$save(function(response) {
+        $location.path('activities/' + response._id);
 
-				// Clear form fields
-				$scope.name = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+        // Clear form fields
+        $scope.name = '';
+      }, function(errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+    };
 
-		// Remove existing Activity
-		$scope.remove = function(activity) {
-			if ( activity ) { 
-				activity.$remove();
+    // Remove existing Activity
+    $scope.remove = function(activity) {
+      if ( activity ) {
+        activity.$remove();
 
-				for (var i in $scope.activities) {
-					if ($scope.activities [i] === activity) {
-						$scope.activities.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.activity.$remove(function() {
-					$location.path('activities');
-				});
-			}
-		};
+        for (var i in $scope.activities) {
+          if ($scope.activities [i] === activity) {
+            $scope.activities.splice(i, 1);
+          }
+        }
+      } else {
+        $scope.activity.$remove(function() {
+          $location.path('activities');
+        });
+      }
+    };
 
-		// Update existing Activity
-		$scope.update = function() {
-			var activity = $scope.activity;
+    // Update existing Activity
+    $scope.update = function() {
+      var activity = $scope.activity;
 
-			activity.$update(function() {
-				$location.path('activities/' + activity._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+      activity.$update(function() {
+        $location.path('activities/' + activity._id);
+      }, function(errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+    };
 
-		// Find a list of Activities
-		$scope.find = function() {
-			$scope.activities = Activities.query();
-		};
+    // Find a list of Activities
+    $scope.find = function() {
+      $scope.activities = Activities.query();
+    };
 
-		// Find existing Activity
-		$scope.findOne = function() {
-			$scope.activity = Activities.get({ 
-				activityId: $stateParams.activityId
-			});
-		};
-	}
+    // Find existing Activity
+    $scope.findOne = function() {
+      $scope.activity = Activities.get({
+        activityId: $stateParams.activityId
+      });
+    };
+  }
 ]);
