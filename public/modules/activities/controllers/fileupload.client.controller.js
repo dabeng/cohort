6441830790440edf3b2de309e67962067bbc26c8
dabeng <xinguan413@163.com>
2015-05-23 10:32:30 +0800

    destroy the fileupload record

--- public/modules/activities/controllers/fileupload.client.controller.js
+++ public/modules/activities/controllers/fileupload.client.controller.js
@@ -1,45 +1,19 @@
 'use strict';
 
 angular.module('activities')
-  .controller('DemoFileUploadController', [ '$scope', '$http', '$filter', '$window',
-      function ($scope, $http) {
-        $scope.$on('fileuploaddone', function(e, data) {
-            if (data.result.error_message) {
-              alert(data.result.error_message);
-            } else {
-              $scope.$parent.attachments = $scope.$parent.attachments.concat(data.result);
-            }
-        });
-
+  .controller('AttachmentUploadCtrl', [ '$scope', '$http', '$filter', '$window', function ($scope, $http) {
+    $scope.$on('fileuploaddone', function(e, data) {
+      if (data.result.error_message) {
+        alert(data.result.error_message);
+      } else {
+        $scope.$parent.attachments = $scope.$parent.attachments.concat(data.result);
+        data.files[0].$destroy();
       }
-  ])
-  .controller('FileDestroyController', [ '$scope', '$http',
-      function ($scope, $http) {
-          var file = $scope.file,
-              state;
-          if (file.url) {
-              file.$state = function () {
-                  return state;
-              };
-              file.$destroy = function () {
-                  state = 'pending';
-                  return $http({
-                      url: file.deleteUrl,
-                      method: file.deleteType
-                  }).then(
-                      function () {
-                          state = 'resolved';
-                          $scope.clear(file);
-                      },
-                      function () {
-                          state = 'rejected';
-                      }
-                  );
-              };
-          } else if (!file.$cancel && !file._index) {
-              file.$cancel = function () {
-                  $scope.clear(file);
-              };
-          }
-      }
-  ]);
+    });
+  }])
+  .controller('FileDestroyController', function ($scope) {
+    var file = $scope.file;
+    file.$destroy = function () {
+      $scope.clear(file);
+    };
+  });
