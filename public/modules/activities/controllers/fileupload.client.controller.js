f8dd5759c11164805f28ff330ba7bcf4993373cc
Dong Xuebin <xudong@dhcp-141-171.nay.redhat.com>
2015-10-09 17:31:11 +0800

    Adjust the style of activity-view page

--- public/modules/activities/controllers/fileupload.client.controller.js
+++ public/modules/activities/controllers/fileupload.client.controller.js
@@ -4,7 +4,7 @@ angular.module('activities')
   .controller('AttachmentUploadCtrl', [ '$scope', '$http', '$filter', '$window', function ($scope, $http) {
     $scope.options = {
       // loadVideoFileTypes: /^mp4$/
-        maxFileSize: 50000000
+        maxFileSize: 100000000
     };
 
     $scope.$on('fileuploadsubmit', function (e, data) {
