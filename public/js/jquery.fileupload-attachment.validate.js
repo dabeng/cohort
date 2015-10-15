99110f5884f5ef99c88fbe9c36fe70a7787df8aa
dabeng <xinguan413@163.com>
2015-10-15 23:13:41 +0800

    Append the image-size validation for attchment upload

--- public/js/jquery.fileupload-attachment.validate.js
+++ public/js/jquery.fileupload-attachment.validate.js
@@ -1,34 +1,56 @@
 $.blueimp.fileupload.prototype.options.processQueue.push(
+  { action: 'imageSize' },
   { action: 'imageDimension' }
 );
 
 $.widget('blueimp.fileupload', $.blueimp.fileupload, {
   processActions: {
-    imageDimension: function (data, options) {
+    imageSize: function (data, options) {
       var dfd = $.Deferred();
       var file = data.files[data.index];
       var imageTypes = /(\.|\/)(gif|jpe?g|png)$/i;
-      var image = new Image();
-      image.onload = function() {
-        if (this.width < 300 || this.height < 300) {
-          file.error = 'Picture should be more than 300x300.';
-        }
-        // if (this.width > 8000 || this.height > 4500) {
-          // if (file.error) {
-          //   file.error += '\nPicture should be less than 8000x4500.';
-          // } else {
-            // file.error = 'Picture should be less than 8000x4500.';
-          // }
-        // }
-        if (file.error) {
+      if (imageTypes.test(file.type)) {
+        var maxSize =  5*1024*1024;
+        if (file.size > maxSize) {
+          file.error = 'Picture should be less than 5M';
           data.files.error = true;
           dfd.rejectWith(this, [data]);
         } else {
           dfd.resolveWith(this, [data]);
         }
-      };
-      image.src = URL.createObjectURL(data.files[0]);
-
+      } else {
+        dfd.resolveWith(this, [data]);
+      }
+      return dfd.promise();
+    },
+    imageDimension: function (data, options) {
+      var dfd = $.Deferred();
+      var file = data.files[data.index];
+      var imageTypes = /(\.|\/)(gif|jpe?g|png)$/i;
+      if (imageTypes.test(file.type)) {
+        var image = new Image();
+        image.onload = function() {
+          if (this.width < 300 || this.height < 300) {
+            file.error = 'Picture should be bigger than 300x300';
+          }
+          if (this.width > 8000 || this.height > 4500) {
+            if (file.error) {
+              file.error += '\nPicture should be bigger than 8000x4500';
+            } else {
+              file.error = 'Picture should be smaller than 8000x4500';
+            }
+          }
+          if (file.error) {
+            data.files.error = true;
+            dfd.rejectWith(this, [data]);
+          } else {
+            dfd.resolveWith(this, [data]);
+          }
+        };
+        image.src = URL.createObjectURL(data.files[0]);
+      } else {
+        dfd.resolveWith(this, [data]);
+      }
       return dfd.promise();
     }
   }
