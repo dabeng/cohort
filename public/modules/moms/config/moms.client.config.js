50cb17de60936c3616a0c339d7006711b289d100
dabeng <zhexiany@gmail.com>
2015-11-08 20:14:23 +0800

    Append the protype of mom module

--- public/modules/moms/config/moms.client.config.js
+++ public/modules/moms/config/moms.client.config.js
@@ -2,10 +2,10 @@
 
 // Configuring the Articles module
 angular.module('moms').run(['Menus',
-	function(Menus) {
-		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Minutes of Meeting', 'moms', 'dropdown', '/moms(/create)?');
-		Menus.addSubMenuItem('topbar', 'moms', 'List Moms', 'moms');
-		Menus.addSubMenuItem('topbar', 'moms', 'New Mom', 'moms/create');
-	}
+  function(Menus) {
+    // Set top bar menu items
+    Menus.addMenuItem('topbar', 'Minutes of Meeting', 'moms', 'dropdown', '/moms(/create)?');
+    Menus.addSubMenuItem('topbar', 'moms', 'List Moms', 'moms');
+    Menus.addSubMenuItem('topbar', 'moms', 'New Mom', 'moms/create');
+  }
 ]);
